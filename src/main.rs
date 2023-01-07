8f2821923a5bd5c21a9163949e9e07209217df63
Mauro M <24578415+Bocanada@users.noreply.github.com>
2023-01-07 08:00:10 -0300

    add telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 use std::net::TcpListener;
 
-use env_logger::Env;
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration.");
 
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
 
-    let connection_pool = PgPool::connect(&config.database.connection_string())
+    let connection_pool = PgPool::connect(config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
